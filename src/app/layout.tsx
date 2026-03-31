d7b645a93fe087f3869673601e52629632c6db3e
SchoolMarm22 <yo@fck.school>
2026-03-30 21:53:48 -0600

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Sidebar } from "@/components/layout/sidebar";
 import { AppFooter } from "@/components/layout/app-footer";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <div className="flex-1">{children}</div>
           <AppFooter />
         </main>
+        <Analytics />
       </body>
     </html>
   );
